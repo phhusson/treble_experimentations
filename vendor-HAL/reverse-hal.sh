def67e0e0690d00585d64d41214729976a646b0a
Pierre-Hugues Husson <phh@phh.me>
2018-01-10 23:07:21 +0100

    [reverse-hal] Support _vendor libs, and support multiply versioned HALs

--- vendor-HAL/reverse-hal.sh
+++ vendor-HAL/reverse-hal.sh
@@ -11,35 +11,39 @@ for HAL in $(xmlstarlet sel -t -m '//hal/name' -v . -n "$1/manifest.xml" |grep -
 		xmlstarlet sel  \
 			-t -m '//hal[./name/text()="'"$HAL"'"]' \
 			-v './interface/name' "$1/manifest.xml")"
-	version="$(
-		xmlstarlet sel \
+	for version in $(xmlstarlet sel \
 			-t -m '//hal[./name/text()="'"$HAL"'"]' \
-			-v './version' "$1/manifest.xml")"
-	class="$(echo "$interface" |sed -E 's/^I/BpHw/g')"
-	namespace="$(echo $HAL |sed -E 's/\./::/g')"
-	namespace2="$(echo ${namespace}::V"$(echo $version |sed -e 's/\./_/g')")"
-	prefix="$namespace2::$class"
+			-v './version' -n "$1/manifest.xml");do
 
-	lib="$(echo "$1"/lib64/${HAL}@${version}.so)"
-	if [ ! -f "$lib" -a -n "$2" ];then
-		lib="$(echo "$2"/lib64/${HAL}@${version}.so)"
-	fi
-	if [ ! -f "$lib" -a -n "$2" ];then
-		lib="$(echo "$2"/system/lib64/${HAL}@${version}.so)"
-	fi
-	if [ ! -f "$lib" ];then
-		echo "Failed to find ${HAL}@${version}.so" >> broken
-		continue
-	fi
-	nm -DC $lib |grep -F "T $prefix" | \
-		sed -E "s/^.*$prefix:://g" | \
-		grep -vE -e '\bping\b' -e '\binterfaceChain\b' -e '\binterfaceDescriptor\b' \
-			-e '\bnotifySyspropsChanged\b' -e '\blinkToDeath\b' -e '\bunlinkToDeath\b' \
-			-e '\bsetHALInstrumentation\b' \
-			-e '\bgetDebugInfo\b' -e '\bdebug\b' \
-			-e '\bgetHashChain\b' -e "^$class\(android::sp<android::hardware::IBinder> const&\)" | \
-		sed -E 's/(, )?std::__1::function<void \((.*)\)>\).*/\) generates \(\2\)/g' | \
-		sed -E 's/android::hardware::hidl_string const\&/string/g' | \
-		sed -E 's/android::hardware::hidl_vec<([^>]*)> const\&/vec<\1>/g' \
-		> $HAL@$version.hal
+		class="$(echo "$interface" |sed -E 's/^I/BpHw/g')"
+		namespace="$(echo $HAL |sed -E 's/\./::/g')"
+		namespace2="$(echo ${namespace}::V"$(echo $version |sed -e 's/\./_/g')")"
+		prefix="$namespace2::$class"
+
+		lib="$(echo "$1"/lib64/${HAL}@${version}.so)"
+		if [ ! -f "$lib" ];then
+			lib="$(echo "$1"/lib64/${HAL}@${version}_vendor.so)"
+		fi
+		if [ ! -f "$lib" -a -n "$2" ];then
+			lib="$(echo "$2"/lib64/${HAL}@${version}.so)"
+		fi
+		if [ ! -f "$lib" -a -n "$2" ];then
+			lib="$(echo "$2"/system/lib64/${HAL}@${version}.so)"
+		fi
+		if [ ! -f "$lib" ];then
+			echo "Failed to find ${HAL}@${version}.so" >> broken
+			continue
+		fi
+		nm -DC $lib |grep -F "T $prefix" | \
+			sed -E "s/^.*$prefix:://g" | \
+			grep -vE -e '\bping\b' -e '\binterfaceChain\b' -e '\binterfaceDescriptor\b' \
+				-e '\bnotifySyspropsChanged\b' -e '\blinkToDeath\b' -e '\bunlinkToDeath\b' \
+				-e '\bsetHALInstrumentation\b' \
+				-e '\bgetDebugInfo\b' -e '\bdebug\b' \
+				-e '\bgetHashChain\b' -e "^$class\(android::sp<android::hardware::IBinder> const&\)" | \
+			sed -E 's/(, )?std::__1::function<void \((.*)\)>\).*/\) generates \(\2\)/g' | \
+			sed -E 's/android::hardware::hidl_string const\&/string/g' | \
+			sed -E 's/android::hardware::hidl_vec<([^>]*)> const\&/vec<\1>/g' \
+			> $HAL@$version.hal
+	done
 done
