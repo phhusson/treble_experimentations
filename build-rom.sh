92b0793a7a619597314223abeec38d703ff32021
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-06-27 18:34:28 +0200

    build-rom.sh: use xz compress for other ROMs

--- build-rom.sh
+++ build-rom.sh
@@ -84,7 +84,7 @@ buildVariant() {
 	make WITHOUT_CHECK_API=true BUILD_NUMBER=$rom_fp installclean
 	make WITHOUT_CHECK_API=true BUILD_NUMBER=$rom_fp -j$jobs systemimage
 	make WITHOUT_CHECK_API=true BUILD_NUMBER=$rom_fp vndk-test-sepolicy
-	cp $OUT/system.img release/$rom_fp/system-${2}.img
+	xz -c $OUT/system.img > release/$rom_fp/system-${2}.img.xz
 }
 
 repo manifest -r > release/$rom_fp/manifest.xml
