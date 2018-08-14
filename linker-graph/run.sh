dfd98b8499bab60bde15aa2afb4a601f225ceb67
Pierre-Hugues Husson <pierre-hugues.husson@softathome.com>
2018-08-14 15:00:49 +0200

    Try again

--- linker-graph/run.sh
+++ linker-graph/run.sh
@@ -6,9 +6,6 @@ parse() {
     if echo "$1" |grep -qE "$ndk";then
         return
     fi
-    if [ -f "$(basename "$1")" ];then
-        return
-    fi
     adb wait-for-device pull "$1" > /dev/null 2>/dev/null
     for so in $(LC_ALL=C readelf -d "$(basename "$1")"  |sed -nE 's/.*NEEDED.*\[([^]]*)\].*/\1/p');do
         if [[ $1 =~ /lib64/ ]];then
@@ -16,10 +13,14 @@ parse() {
         else
             lib=lib
         fi
+        if [ -f "$so" ];then
+            echo "File already parsed, skipping"
+            echo -e "\t${2}${1}"
+            continue
+        fi
         found=false
         for path in /odm/$lib /vendor/$lib /system/$lib/vndk-sp-26 /system/$lib;do
             if adb wait-for-device pull "$path"/"$so"  > /dev/null 2>/dev/null;then
-                rm -f "$so"
                 parse "$path"/"$so" "${2}\t"
                 found=true
                 break
