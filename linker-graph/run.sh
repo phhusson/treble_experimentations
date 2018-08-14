89d094802a85855db35099923b6b49481469a188
Pierre-Hugues Husson <pierre-hugues.husson@softathome.com>
2018-08-14 13:21:46 +0200

    Add a script to display dependency graph of a lib

---
+++ linker-graph/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ndk="(liblog.so|libdl.so|ld-android.so|libm.so|libc.so)"
+parse() {
+    echo -e "${2}${1}"
+    if echo "$1" |grep -qE "$ndk";then
+        return
+    fi
+    adb pull "$1" > /dev/null 2>/dev/null
+    for so in $(LC_ALL=C readelf -d "$(basename "$1")"  |sed -nE 's/.*NEEDED.*\[([^]]*)\].*/\1/p');do
+        if [[ $1 =~ /lib64/ ]];then
+            lib=lib64
+        else
+            lib=lib
+        fi
+        found=false
+        for path in /odm/$lib /vendor/$lib /system/$lib/vndk-sp-26 /system/$lib;do
+            if adb pull "$path"/"$so"  > /dev/null 2>/dev/null;then
+                parse "$path"/"$so" "${2}\t"
+                found=true
+                break
+            fi
+        done
+        if ! $found;then
+            echo "Lib $so not found"
+        fi
+    done
+}
+
+tmp="$(mktemp -d)"
+pushd "$tmp"
+parse "$1" ""
+popd
+rm -Rf $tmp
