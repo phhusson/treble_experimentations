6647c1b390c59d6ee434c64d78a7f20b05a0bbd8
Zadkiel <hello@zadkiel.fr>
2018-12-07 21:38:15 +0100

    build-dakkar: update AOSP versions
    aosp81: r30 => r48
    aosp90: r1 => r21

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -85,14 +85,14 @@ function get_rom_type() {
                 ;;
             aosp81)
                 mainrepo="https://android.googlesource.com/platform/manifest.git"
-                mainbranch="android-8.1.0_r30"
+                mainbranch="android-8.1.0_r48"
                 localManifestBranch="android-8.1"
                 treble_generate=""
                 extra_make_options=""
                 ;;
             aosp90)
                 mainrepo="https://android.googlesource.com/platform/manifest.git"
-                mainbranch="android-9.0.0_r1"
+                mainbranch="android-9.0.0_r21"
                 localManifestBranch="android-9.0"
                 treble_generate=""
                 extra_make_options=""
