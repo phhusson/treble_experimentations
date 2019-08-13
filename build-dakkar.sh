ae582ee0d304570a33ea2b72febac7147f7b6459
bzntm <bertitmati@hotmail.com>
2019-08-13 12:27:19 +0700

    Update base ROM
    Update ROM to Pie, why still stuck on oreo branch?

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -106,8 +106,8 @@ function get_rom_type() {
                 ;;
             carbon)
                 mainrepo="https://github.com/CarbonROM/android.git"
-                mainbranch="cr-6.1"
-                localManifestBranch="android-8.1"
+                mainbranch="cr-7.0"
+                localManifestBranch="android-9.0"
                 treble_generate="carbon"
                 extra_make_options="WITHOUT_CHECK_API=true"
                 ;;
@@ -134,8 +134,8 @@ function get_rom_type() {
                 ;;
             rr)
                 mainrepo="https://github.com/ResurrectionRemix/platform_manifest.git"
-                mainbranch="oreo"
-                localManifestBranch="android-8.1"
+                mainbranch="pie"
+                localManifestBranch="android-9.0"
                 treble_generate="rr"
                 extra_make_options="WITHOUT_CHECK_API=true"
                 ;;
@@ -155,29 +155,29 @@ function get_rom_type() {
                 ;;
             crdroid)
                 mainrepo="https://github.com/crdroidandroid/android.git"
-                mainbranch="8.1"
-                localManifestBranch="android-8.1"
+                mainbranch="9.0"
+                localManifestBranch="android-9.0"
                 treble_generate="crdroid"
                 extra_make_options="WITHOUT_CHECK_API=true"
                 ;;
             mokee)
                 mainrepo="https://github.com/MoKee/android.git"
-                mainbranch="mko-mr1"
-                localManifestBranch="android-8.1"
+                mainbranch="mkp"
+                localManifestBranch="android-9.0"
                 treble_generate="mokee"
                 extra_make_options="WITHOUT_CHECK_API=true"
                 ;;
             aicp)
                 mainrepo="https://github.com/AICP/platform_manifest.git"
-                mainbranch="o8.1"
-                localManifestBranch="android-8.1"
+                mainbranch="p9.0"
+                localManifestBranch="android-9.0"
                 treble_generate="aicp"
                 extra_make_options="WITHOUT_CHECK_API=true"
                 ;;
             aokp)
                 mainrepo="https://github.com/AOKP/platform_manifest.git"
-                mainbranch="oreo"
-                localManifestBranch="android-8.1"
+                mainbranch="pie"
+                localManifestBranch="android-9.0"
                 treble_generate="aokp"
                 extra_make_options="WITHOUT_CHECK_API=true"
                 ;;
@@ -190,8 +190,8 @@ function get_rom_type() {
                 ;;
             slim)
                 mainrepo="https://github.com/SlimRoms/platform_manifest.git"
-                mainbranch="or8.1"
-                localManifestBranch="android-8.1"
+                mainbranch="9.0"
+                localManifestBranch="android-9.0"
                 treble_generate="slim"
                 extra_make_options="WITHOUT_CHECK_API=true"
                 ;;
