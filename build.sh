c88fb398191de23be4f0326c28a79a36691d960b
Dualkem <55616055+Dualkem@users.noreply.github.com>
2020-07-15 18:16:18 +0300

    Added comments to build.sh
    Make the script nicer

--- build.sh
+++ build.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 rom_fp="$(date +%y%m%d)"
+# Remember the folder in which this script is (treble_experimentations)
 originFolder="$(dirname "$0")"
+# Create output folder
 mkdir -p release/$rom_fp/
+# If there are errors, exit immediately
 set -e
 
+#Set USER variable if it does not exist
 if [ -z "$USER" ];then
 	export USER="$(id -un)"
 fi
+# No idea
 export LC_ALL=C
 
+# Set initial repo parameters
 manifest_url="https://android.googlesource.com/platform/manifest"
 aosp="android-8.1.0_r65"
 phh="android-8.1"
 
+# Read the first argument and set the repo arguments accordingly
 if [ "$1" == "android-9.0" ];then
     manifest_url="https://gitlab.com/aosp-security/manifest"
     aosp="android-9.0.0_r53-r47"
@@ -24,39 +31,59 @@ elif [ "$1" == "android-10.0" ];then
     phh="android-10.0"
 fi
 
+# Does nothing for us
 if [ "$release" == true ];then
     [ -z "$version" ] && exit 1
     [ ! -f "$originFolder/release/config.ini" ] && exit 1
 fi
 
+# Repo init the selected Android Source Branch
 repo init -u "$manifest_url" -b $aosp
+# Did we clone phh's treble manifests?
 if [ -d .repo/local_manifests ] ;then
 	( cd .repo/local_manifests; git fetch; git reset --hard; git checkout origin/$phh)
+# If not then we will, and we'll place them in the local_manifests folder
 else
 	git clone https://github.com/phhusson/treble_manifest .repo/local_manifests -b $phh
 fi
+# Download the source files according to the manifests
 repo sync -c -j 1 --force-sync
 
+# Download GAPPS (Google Apps)
 repo forall -r '.*opengapps.*' -c 'git lfs fetch && git lfs checkout'
+# Change directory to treble device and generate the mk flavors
 (cd device/phh/treble; git clean -fdx; bash generate.sh)
+# Download the FLOSS apps to use in the ROM
 (cd vendor/foss; git clean -fdx; bash update.sh)
+# Remove some GAPPS things
 rm -f vendor/gapps/interfaces/wifi_ext/Android.bp
 
+# Prepare the build environment
 . build/envsetup.sh
 
+# Define the buildVariant function for later use
 buildVariant() {
+	# Execute lunch command with the BUILD FLAVOR argument
 	lunch $1
+	# Execute make to compile some of the ROM's components
 	make BUILD_NUMBER=$rom_fp installclean
 	make BUILD_NUMBER=$rom_fp -j8 systemimage
 	make BUILD_NUMBER=$rom_fp vndk-test-sepolicy
+	# Pack everything nicely into a tar.xz archive for reducing the size of the resulting images
 	xz -c $OUT/system.img -T0 > release/$rom_fp/system-${2}.img.xz
 }
-
+# Create a manifest
 repo manifest -r > release/$rom_fp/manifest.xml
+# Execute the list-patches script from the cloned treble_experimentations folder
 bash "$originFolder"/list-patches.sh
+# Take the patches.zip archive and copy it into the output folder
 cp patches.zip release/$rom_fp/patches.zip
 
+# Decide what kinds of system images we will build
 if [ "$1" = "android-10.0" ];then
+	# Call the buildVariant function and pass the build flavor parameter
+	# TIP: You can comment the flavors that you don't want
+	#  COMMAND           BUILD FLAVOR           OUTPUT FILE NAME
 	buildVariant treble_arm64_afS-userdebug quack-arm64-aonly-floss
 	buildVariant treble_arm64_avS-userdebug quack-arm64-aonly-vanilla
 	buildVariant treble_arm64_agS-userdebug quack-arm64-aonly-gapps
@@ -104,6 +131,7 @@ else
 	rm -Rf out/target/product/phhgsi*
 fi
 
+# Does nothing for us
 if [ "$release" == true ];then
     (
         rm -Rf venv
