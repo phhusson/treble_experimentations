4a95e7a81499a39bc58844edfa54d94e5c5f42d9
Pierre-Hugues Husson <phh@phh.me>
2021-01-06 11:50:51 +0100

    Google didnt push r28 manifest... use r27 instead (it is same version code)

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ elif [ "$1" == "android-10.0" ];then
     phh="android-10.0"
 elif [ "$1" == "android-11.0" ];then
     manifest_url="https://android.googlesource.com/platform/manifest"
-    aosp="android-11.0.0_r28"
+    aosp="android-11.0.0_r27"
     phh="android-11.0"
 fi
 
