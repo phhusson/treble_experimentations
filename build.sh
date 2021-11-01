74318e1804781ab1b2386f5ee749a1890a2e0e10
Pierre-Hugues Husson <phh@phh.me>
2021-11-01 22:17:03 +0100

    Up 12 to r11

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ elif [ "$1" == "android-11.0" ];then
     phh="android-11.0"
 elif [ "$1" == "android-12.0" ];then
     manifest_url="https://android.googlesource.com/platform/manifest"
-    aosp="android-12.0.0_r7"
+    aosp="android-12.0.0_r11"
     phh="android-12.0"
 else
 	# guess android version from version number
